847c80c308bbc30aa24fc542a21d0406859e9096
Alrighter <59889888+Alrighter@users.noreply.github.com>
2021-04-28 00:47:31 +0300

    Initial commit

---
+++ AntivirusVS113dikthreesome/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+
+
+namespace AntivirusVS113dikthreesome
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private RemoveVirusWindow _window1;
+
+        private void RemoveVirusButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            _window1 = new RemoveVirusWindow();
+            _window1.Show();
+
+        }
+
+        private void SecurityStatusButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (RemoveVirusWindow.Active)
+            {
+                MessageBox.Show("Ваш комп'ютер під захистом!");
+            }
+            else
+            {
+                MessageBox.Show("Рекомендуємо ввімкнити захист! \n\n\nВін увімкнеться автоматично після перевірки та знищення потенційних загроз на вашому комп'ютері!");
+            }
+        }
+
+        private void InfoButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Створено для запобігання появи нових та знешкодження старих вірусів пов'язаних з діяльністю рабі та його шайки\n\nby https://t.me/alrighter \n\nЗа підтримки https://t.me/theporoshenko \n\n                                                Version 1.0 (release)");
+
+        }
+    }
+}
---
+++ AntivirusVS113dikthreesome/RemoveVirusWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿
+namespace AntivirusVS113dikthreesome
+{
+    public partial class RemoveVirusWindow
+    {
+        private static bool _active = false;
+
+        public static bool Active
+        {
+            get { return _active; }
+            set { _active = value; }
+        }
+
+        public RemoveVirusWindow()
+        {
+            InitializeComponent();
+            _active = true;
+        }
+    }
+}
